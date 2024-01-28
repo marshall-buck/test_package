e304946039b672644549a596f4d9763781baa6ad
Marshall Buck <dev@marshallbuck.com>
2024-01-28 16:04:43 -0600

    updated fileservice  to asset path instead of file path

--- lib/src/file_service.dart
+++ lib/src/file_service.dart
@@ -1,15 +1,31 @@
 import 'package:path/path.dart' as p;
 import 'package:flutter/services.dart' show rootBundle;
 
-/// File service class.
+// /// File service class.
+// class FileService {
+//   final pathToFiles = p.join('packages', 'test_package', 'lib', 'data');
+//   // final pathToFiles = p.join('lib', 'data');
+
+//   /// Opens a file from [fileName].  Returns the contents as a [String].
+//   Future<String> loadData({required final String fileName}) async {
+//     final String contents =
+//         await rootBundle.loadString(p.join(pathToFiles, fileName));
+
+//     return contents;
+//   }
+// }
+
 class FileService {
+  // Correct the path format to use forward slashes for asset loading
+  // final pathToFiles = 'packages/test_package/lib/data';
   final pathToFiles = p.join('packages', 'test_package', 'lib', 'data');
-  // final pathToFiles = p.join('lib', 'data');
 
   /// Opens a file from [fileName].  Returns the contents as a [String].
   Future<String> loadData({required final String fileName}) async {
-    final String contents =
-        await rootBundle.loadString(p.join(pathToFiles, fileName));
+    // Ensure the use of forward slashes in the asset path
+    final String assetPath =
+        p.join(pathToFiles, fileName).replaceAll(r'\', '/');
+    final String contents = await rootBundle.loadString(assetPath);
 
     return contents;
   }
