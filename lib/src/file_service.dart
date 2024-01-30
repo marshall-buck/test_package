94b9564ad5ca71d904471748f2a69583b4c6ed70
Marshall Buck <dev@marshallbuck.com>
2024-01-30 15:31:33 -0600

    Delete test.json and update fileService.dart to get file hash prefix

--- lib/src/file_service.dart
+++ lib/src/file_service.dart
@@ -1,13 +1,19 @@
+import 'dart:io';
+import 'package:path/path.dart' as p;
+
 import 'package:flutter/services.dart' show rootBundle;
 
 class FileService {
   static const String _dataPath = 'packages/test_package/lib/data';
 
+  get fileHash => _getFileHash();
+
   /// Opens a file from [fileName] located in the package's assets.
   /// Returns the contents as a [String].
   Future<String> loadData({required String fileName}) async {
-    final String assetPath = '$_dataPath/$fileName';
+    final String assetPath = '$_dataPath/${fileHash}_$fileName';
     final String contents;
+    print('LOAD DATA: $assetPath  fileHash: $fileHash');
     try {
       contents = await rootBundle.loadString(assetPath);
     } catch (e) {
@@ -17,5 +23,16 @@ class FileService {
     }
 
     return contents;
+  }
+
+  ///Checks data directory for file names, and creates the hash prefix for the file name.
+  String _getFileHash() {
+    final path = p.join('lib', 'data');
+    final Directory dataDir = Directory(path);
+    final List<FileSystemEntity> files = dataDir.listSync(followLinks: false);
+    if (files.isEmpty) {
+      throw FileSystemException('getFileHash() - No files found', path);
+    }
+    return files[0].path.split('/').last.split('_').first;
   }
 }
