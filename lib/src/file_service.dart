96c4d3dc6dda628cc527e9dc8be275b0ff09516e
Marshall Buck <dev@marshallbuck.com>
2024-01-30 16:47:57 -0600

    Delete test.json and update file_service.dart to load data with a file hash prefix

--- lib/src/file_service.dart
+++ lib/src/file_service.dart
@@ -2,24 +2,36 @@ import 'package:flutter/services.dart' show rootBundle;
 
 class FileService {
   static const String _dataPath = 'packages/test_package/lib/data';
-
-  // get fileHash => _getFileHash();
+  static const String _fileNameManifest = 'file_manifest.txt';
 
   /// Opens a file from [fileName] located in the package's assets.
   /// Returns the contents as a [String].
   Future<String> loadData({required String fileName}) async {
-    final String assetPath = '$_dataPath/$fileName';
+    final fileHash = await _getFileHash();
+    final String assetPath = '$_dataPath/${fileHash}_$fileName';
     final String contents;
 
     try {
       contents = await rootBundle.loadString(assetPath);
     } catch (e) {
       // If there's an error loading the asset, print the error and the attempted path
-      // print('Error loading asset at $assetPath: $e');
+      print('Error loading file at $assetPath: $e');
       rethrow;
     }
 
     return contents;
+  }
+
+  Future<String> _getFileHash() async {
+    try {
+      final String manifest =
+          await rootBundle.loadString('$_dataPath/$_fileNameManifest');
+      return manifest;
+    } catch (e) {
+      // If there's an error loading the asset, print the error and the attempted path
+      print('Error loading manifest at $_dataPath/$_fileNameManifest: $e');
+      rethrow;
+    }
   }
 
   ///Checks data directory for file names, and creates the hash prefix for the file name.
